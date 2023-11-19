04c2e95cab7899a6fd7dd39db6ab57c821e37f43
tonimeshm <tonimeshm@gmail.com>
2023-11-20 02:29:15 +0530

    added base path

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -18,14 +18,9 @@ function Hero({heading, linkedin, github, gitlab,id}) {
           }
         } aria-hidden="true" focusable="false">
           <linearGradient id="my-light-gradient" x2="1" y2="1">
-          {/* #000000 -43.27%,#000000 -21.24%,#525252 12.19%,#4f4f4f 29.82%,#27260f 51.94%,#cfa033 90.29% */}
-
-            <stop offset="-43.27%" stopColor="#000000" />
-            <stop offset="-21.24%" stopColor="#000000" />
-            <stop offset="12.19%" stopColor="#525252" />
-            <stop offset="29.82%" stopColor="#4f4f4f" />
-            <stop offset="51.94%" stopColor="#27260f" />
-            <stop offset="90.29%" stopColor="#cfa033" />
+          {/* linear-gradient(to right, #F37335, #FDC830); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */}
+            <stop offset="-43.27%" stopColor="#FDC830" />
+            <stop offset="90.29%" stopColor="#F37335" />
           </linearGradient>
           <linearGradient id="my-dark-gradient" x2="1" y2="1">
           {/* linear-gradient(157.81deg,#faf7de -43.27%,#ecf5be -21.24%,#eef09d 12.19%,#e0eb7d 29.82%,#e6e25c 51.94%,#cfa033 90.29%); */}
